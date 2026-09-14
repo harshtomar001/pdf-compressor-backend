b6175057502c976c55323a9de324fcaefa61cbff
Harsh Tomar <harshtomar4081@gmail.com>
2026-09-14 23:50:00 +0530

    feat: track job creation time

--- Models/PdfJob.cs
+++ Models/PdfJob.cs
@@ -5,10 +5,17 @@ namespace pdf_compressor.Models;
 public class PdfJob
 {
     public string JobId { get; set; } = "";
+    
     public string InputPath { get; set; } = "";
+    
     public string OutputPath { get; set; } = "";
+    
     public JobStatus Status { get; set; } = JobStatus.Queued;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
     public string CompressionEngine { get; set; } = "ghostscript";
+    
     public CompressionOptions Compression { get; set; } = new();
 
     [JsonIgnore]
